2f350f7b6d0f84c99a4385e8f421ef9ff3f0ad7c
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-10-12 11:17:44 +0300

    + cqrs mediatr

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,7 +1,51 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
 {
     //Application Logic Layer
-    public class CreateProductHandler
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //logic to create product
+
+            //create entity
+            var product = new Product
+            {
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+                Name = command.Name,
+            };
+            //save object
+
+            //return CreateProductResult
+            return new CreateProductResult(Guid.NewGuid());
+        }
     }
+
+    /* 
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //logic to create product
+            throw new NotImplementedException();
+        }
+    }
+
+    */
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,9 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services DI
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 //configures the http request pipeline.
 
-
+app.MapCarter();
 app.Run();
