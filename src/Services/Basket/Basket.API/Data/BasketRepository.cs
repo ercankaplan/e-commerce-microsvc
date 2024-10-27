835c713a93a1eb714c1d238cb536ef17993579a1
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-10-27 23:00:14 +0300

    added basket api

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using Basket.API.Exceptions;
+using Marten;
+
+namespace Basket.API.Data
+{
+    public class BasketRepository(IDocumentSession dbSession) : IBasketRepository
+    {
+
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var basket = await dbSession.LoadAsync<ShoppingCart>(userName,cancellationToken);
+
+            return basket is null ? throw new BasketNotFoundException("Basket", userName) : basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            dbSession.Store<ShoppingCart>(basket);
+
+            await dbSession.SaveChangesAsync(cancellationToken);
+
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            dbSession.Delete<ShoppingCart>(userName);
+
+            await dbSession.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+
+       
+    }
+}
