4e9e752f15034a5ec4c4106c847643a2697c3a54
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-10-28 15:51:50 +0300

    added Redis distributed cache

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+    {
+
+
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+
+            if (!string.IsNullOrEmpty(cachedBasket))
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
+
+            var basket = await repository.GetBasket(userName, cancellationToken);
+
+            if (basket is not null)
+                await cache.SetStringAsync(userName, System.Text.Json.JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            var storedBasket = await repository.StoreBasket(basket, cancellationToken);
+
+            if (storedBasket is not null)
+                await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(storedBasket), cancellationToken);
+
+            return storedBasket;
+        }
+
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var isDeleted = await repository.DeleteBasket(userName, cancellationToken);
+
+            if(isDeleted)
+                await cache.RemoveAsync(userName, cancellationToken);
+
+            return isDeleted;
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using Basket.API.Data;
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Handlers;
 using Marten;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
 var connStrBasketDB = builder.Configuration.GetConnectionString("BasketDB");
+var redisConStr = builder.Configuration.GetConnectionString("Redis");
 
 
 builder.Services.AddMediatR(config=> {
@@ -30,6 +32,24 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+/* manually DI CachedBasketRepository
+builder.Services.AddScoped(provider=> {
+
+    var basketRepository = provider.GetRequiredService<BasketRepository>();
+    var distributedCache = provider.GetRequiredService<IDistributedCache>();
+
+    return new CachedBasketRepository(basketRepository, distributedCache);
+
+});
+*/
+builder.Services.Decorate<IBasketRepository,CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "BasketAPICache";
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
