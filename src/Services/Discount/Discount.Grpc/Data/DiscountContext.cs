88b39aef6bb3fc910b311d2cc973fba3b183c21a
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-11-05 23:21:38 +0300

    added discount service

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,25 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+
+    //DATA ACCESS LAYER
+    public class DiscountContext:DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> options):base(options) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1 , ProductName = "IPhone 13", Description = "IPhone 13 discounting", Amount = 50 },
+                 new Coupon { Id = 2, ProductName = "Samsung Galaxy S21", Description = "Samsung Galaxy S21 discounting", Amount = 100 }
+                );
+        }
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+    }
+}
