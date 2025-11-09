2eb69e6300394ac0b542e85caa2038c765a1926c
ercan kaplan <ercan.kaplan@setur.com.tr>
2025-11-09 13:04:44 +0300

    added domain entities valueobject, aggregates

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,29 @@
+﻿using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Domain.Models
+{
+    public class OrderItem:Entity<OrderItemId>
+    {
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {
+            Id = OrderItemId.Of(Guid.NewGuid());
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+        public OrderId OrderId { get; private set; } = default!;
+        public ProductId ProductId { get; private set; } = default!;
+        public int Quantity { get; private set; } = default!;
+        public decimal Price { get; private set; } = default!;
+
+        public static OrderItem Create(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegative(price);
+
+            return new OrderItem(orderId, productId, quantity, price);
+        }
+
+    }
+}
